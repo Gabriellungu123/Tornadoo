e80178d386de59036ab8ec989f991287c9712c18
Gabriellungu123 <gabilungu913@gmail.com>
2025-12-23 10:26:57 +0100

    Segundo pasiito

--- main.py
+++ main.py
@@ -13,11 +13,34 @@ def make_app():
     static_path="static")       # carpeta donde está el CSS
 
 
+class SubmitHandler(tornado.web.RequestHandler):
+    def post(self):
+        usuario = self.get_argument("nombre")
+        email = self.get_argument("email")
+
+        # Aquí podrías validar login, guardar datos, etc.
+
+        self.redirect("/pagina")
+
+
+class PaginaHandler(tornado.web.RequestHandler):
+    def get(self):
+        self.render("pagina.html")
+
 
 async def main():
     app = make_app()
     app.listen(8888)
     await asyncio.Event().wait()
+
+def make_app():
+    return tornado.web.Application([
+        (r"/", FormHandler),
+        (r"/submit", SubmitHandler),
+        (r"/pagina", PaginaHandler),
+    ],
+    template_path="templates",
+    static_path="static")
 
 if __name__ == "__main__":
     asyncio.run(main())
